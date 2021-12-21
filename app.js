2306e542d9e3bf9bb49502bc4249480904c0de8f
Davi Chagas <davi.a.chagas@unesp.br>
2021-12-21 09:48:48 -0300

    JS file now modify the HTML, through the DOM

--- app.js
+++ app.js
@@ -1,45 +1,112 @@
-let numberUserWon = 0
+let numberPlayerWon = 0
 let numberComputerWon = 0
+
+const roundMessage = document.querySelector('.roundMessage');
+
+const optionButtons = document.querySelectorAll('.optionButton');
+
+const displayOfPlayerScore = document.querySelector('.playerScore');
+const displayOfComputerScore = document.querySelector('.computerScore');
+
+const buttonToRestart = document.querySelector('#restart');
+
+showPlayerWins = (playerSelection, computerSelection) => {
+
+
+    if ((playerSelection === 'rock') && (computerSelection === 'scissors')) {
+        roundMessage.textContent = 'Rock beats scissors, you won!'
+         numberPlayerWon += 1
+     }
+ 
+     if ((playerSelection === 'paper') && (computerSelection === 'rock')) {
+        roundMessage.textContent = 'Paper beats rock, you won!'
+         numberPlayerWon += 1
+     }
+ 
+     if ((playerSelection === 'scissors') && (computerSelection === 'paper')) {
+        roundMessage.textContent = 'Scissors beats paper, you won!'
+         numberPlayerWon += 1
+     }
+}
+
+showComputerWins = (playerSelection, computerSelection) => {
+    if ((computerSelection === 'rock') && (playerSelection === 'scissors')) {
+        roundMessage.textContent = 'Rock beats scissors, computer won!'
+         numberComputerWon += 1
+     }
+ 
+     if ((computerSelection === 'paper') && (playerSelection === 'rock')) {
+        roundMessage.textContent = 'Paper beats rock, computer won!'
+         numberComputerWon += 1
+     }
+ 
+     if ((computerSelection === 'scissors') && (playerSelection === 'paper')) {
+        roundMessage.textContent = 'Scissors beats paper, computer won!'
+         numberComputerWon += 1
+     }
+}
+
 
 playRound = (playerSelection, computerSelection) => {
 
-    if ((playerSelection === 'rock') && (computerSelection === 'scissors')) {
-        console.log('Rock beats scissors, you won!')
-        numberUserWon += 1
-    }
+    let scoreComparatorForPlayer = numberPlayerWon
+    let scoreComparatorForPc = numberComputerWon
 
-    if ((playerSelection === 'paper') && (computerSelection === 'rock')) {
-        console.log('Paper beats rock, you won!')
-        numberUserWon += 1
-    }
+    const buttonsToDisable = document.querySelectorAll('.optionButton')
 
-    if ((playerSelection === 'scissors') && (computerSelection === 'paper')) {
-        console.log('Scissors beats paper, you won!')
-        numberUserWon += 1
-    }
+    const iconSelectedByPlayer = document.querySelector(`#${playerSelection}SVG`)
+    const iconSelectedByPc = document.querySelector(`#${computerSelection}SVG`)
+
+    const SVGPathSelectedByPlayer = document.querySelector(`.${playerSelection}Path`).style
+    const SVGPathSelectedByPc = document.querySelector(`.${computerSelection}Path`).style
+
+    showPlayerWins(playerSelection, computerSelection)
+    showComputerWins(playerSelection, computerSelection)
+
+    buttonsToDisable.forEach(buttonToDisable => {
+        buttonToDisable.disabled = true
+    })
+
+    iconSelectedByPlayer.classList.add('animationClass')
+    iconSelectedByPc.classList.add('animationClass')
+    
+
+    scoreComparatorForPlayer === numberPlayerWon ? 
+    SVGPathSelectedByPlayer.fill = '#DE4242': 
+    SVGPathSelectedByPlayer.fill = '#47A52F'
+
+    scoreComparatorForPc === numberComputerWon ? 
+    SVGPathSelectedByPc.fill = '#DE4242': 
+    SVGPathSelectedByPc.fill = '#47A52F'
 
 
     if (playerSelection === computerSelection) {
-        console.log('It\'s a tie!')
-    }
-
-
-    if ((computerSelection === 'rock') && (playerSelection === 'scissors')) {
-        console.log('Rock beats scissors, computer won!')
-        numberComputerWon += 1
-    }
-
-    if ((computerSelection === 'paper') && (playerSelection === 'rock')) {
-        console.log('Paper beats rock, computer won!')
-        numberComputerWon += 1
-    }
-
-    if ((computerSelection === 'scissors') && (playerSelection === 'paper')) {
-        console.log('Scissors beats paper, computer won!')
-        numberComputerWon += 1
-    }
-
+        roundMessage.textContent = 'It\'s a tie!'
+        document.querySelector(`.${playerSelection}Path`).style.fill = "#2AC0CA"
+     }
+     
+    displayOfPlayerScore.textContent = `You: ${numberPlayerWon}`
+    displayOfComputerScore.textContent = `Computer: ${numberComputerWon}`
 }
+
+setInterval(() => {  
+    const SVGs = document.querySelectorAll('svg')
+    const SVGsPath = document.querySelectorAll('.path')
+    const buttonsToAble = document.querySelectorAll('.optionButton')
+
+    SVGs.forEach(SVG => {
+        SVG.classList.remove('animationClass')
+    });
+
+    SVGsPath.forEach(path => {
+        path.style.fill = "#000000"
+    })
+    
+    buttonsToAble.forEach(buttonToAble => {
+        buttonToAble.disabled = false
+    })
+    
+}, 2000)
 
 
 let optionsArray = ['rock', 'paper', 'scissors']
@@ -48,26 +115,66 @@ computerPlay = () => {
     return choiceOfComputer = optionsArray[Math.floor(Math.random() * optionsArray.length)]
 }
 
+displayResultMessage = (boolean) => {
+    const displayMessage = document.querySelector('.resultMessage')
+    const blurAtTheScreen = document.querySelector('.blurApplication')
 
-game = () => {
+    const whoWon = document.querySelector('.whoWon')
+    const finalScoreBoard = document.querySelector('.finalScoreBoard')
 
-const playerSelection = prompt("Enter with your choice").toLocaleLowerCase();
-const computerSelection = computerPlay()
-console.log(`Player: ${playerSelection}\nComputer: ${computerSelection}`)
 
-playRound(playerSelection, computerSelection)
+    whoWon.textContent = numberPlayerWon > numberComputerWon ? 'Congrats, you won! 🥳' 
+    : 'Computer won! :('
+
+    finalScoreBoard.textContent = `You: ${numberPlayerWon} X  Computer: ${numberComputerWon}`
+
+
+    if(boolean === true) {
+    displayMessage.style.display = 'flex'
+    blurAtTheScreen.style.filter = 'blur(8px)'
+    } 
+
+    if(boolean === false) {
+        displayMessage.style.display = 'none'
+        blurAtTheScreen.style.filter = 'none'
+    }
+
+    if(boolean === 'ignore it') {
+        displayMessage.style.display = 'none'
+        blurAtTheScreen.style.filter = 'none'
+    }
 
 }
 
-let index = 0
-while(index < 5) {
-    game()
-    index++
+restartTheGame = () => {
+    numberPlayerWon = 0
+    numberComputerWon = 0
+
+    displayResultMessage(false)
+}
+ 
+buttonToRestart.addEventListener('click', restartTheGame)
+
+getResultMessage = (numberPlayerWon, numberComputerWon) => {
+    if ((numberPlayerWon === 5) || (numberComputerWon === 5)) {
+        displayResultMessage(true)
+ }
 }
 
-console.log(`User won ${numberUserWon} times
-Computer won ${numberComputerWon} times
-There was a tie ${5 - (numberComputerWon + numberUserWon)} times
 
 
-${numberComputerWon > numberUserWon ? "Computer" : "User"} won!`)
+game = (selectedButton) => {
+    const playerSelection = selectedButton.getAttribute('id')
+
+    const computerSelection = computerPlay()
+
+     playRound(playerSelection, computerSelection);
+
+        getResultMessage(numberPlayerWon, numberComputerWon)
+   }
+
+
+
+optionButtons.forEach((button) => {
+    button.addEventListener('click', () => game(button));
+  });
